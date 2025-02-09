5c888367a695fe8b68a95f0ef7b33ac8579d2dca
Zhiwei(Lucas) Li <lucas.lizhiwei@gmail.com>
2025-02-09 17:17:44 -0500

    Refresh on trade page added

--- src/components/OpenOrder.jsx
+++ src/components/OpenOrder.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Box, Typography, TextField, MenuItem, Button, Slider, Autocomplete } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import { Box, Typography, TextField, MenuItem, Button, Slider, Autocomplete, IconButton } from '@mui/material';
 
-const OpenOrder = ({ sector, handleSectorChange, symbol, handleSymbol, symbolList, symbolLeverages, leverage, setLeverage, size, setSize, available, handleTrade, price}) => {
+const OpenOrder = ({ sector, handleSectorChange, symbol, handleSymbol, symbolList, symbolLeverages, leverage, setLeverage, size, setSize, available, handleTrade, price, refreshData}) => {
 
   return (
     <Box sx={{ maxWidth: '50%', margin: '0 auto', background: '#fff', padding: '30px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'left', alignItems: 'center', marginBottom: '20px' }}>
+          <Typography variant="h7">
+              Clean Input
+          </Typography>
+          <IconButton sx={{color: '#7E57C2'}} onClick={refreshData}>
+              <RefreshIcon />
+          </IconButton>
+      </Box>
       <TextField
           select
           label="Sector"
