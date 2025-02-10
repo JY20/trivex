1c5916389839e6dbea722b3153a1bac45d766846
JY20 <jimmyyan1337@gmail.com>
2025-02-10 09:52:42 -0500

    updating close order

--- src/components/CloseOrder.jsx
+++ src/components/CloseOrder.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Typography, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 const CloseOrder = ({ positions, handleCloseOrder }) => {
+  console.log(positions);
   return (
     <Box sx={{ maxWidth: '50%', marginTop: '30px', padding: '20px', backgroundColor: '#f1f1f1', borderRadius: '8px', margin: '20px auto' }}>
       <Typography variant="h5" sx={{ marginBottom: '20px' }}>
@@ -11,6 +12,7 @@ const CloseOrder = ({ positions, handleCloseOrder }) => {
         <Table>
           <TableHead>
             <TableRow>
+            <TableCell><strong>Sector</strong></TableCell>
               <TableCell><strong>Symbol</strong></TableCell>
               <TableCell align="right"><strong>Quantity</strong></TableCell>
               <TableCell align="right"><strong>Average Price</strong></TableCell>
@@ -20,6 +22,7 @@ const CloseOrder = ({ positions, handleCloseOrder }) => {
           <TableBody>
             {positions.map((position, index) => (
               <TableRow key={index}>
+                <TableCell>{position.sector}</TableCell>
                 <TableCell>{position.symbol}</TableCell>
                 <TableCell align="right">{position.quantity}</TableCell>
                 <TableCell align="right">{position.average_price}</TableCell>
